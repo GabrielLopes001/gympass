3a7e57b82afc6f5b2e2e63d625388b7329054276
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-09 21:52:46 -0300

    feat: criando componente de input

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+import { color, useTheme } from "@shopify/restyle";
+import { useState } from "react";
+import { TextInput, TextInputProps } from "react-native";
+import { ThemeProps } from "src/theme";
+
+export function Input({...rest}: TextInputProps){
+  const theme = useTheme<ThemeProps>();
+  const [focus, setFocus] = useState(false);
+
+  return(
+    <TextInput
+      style={{
+        backgroundColor:theme.colors.gray_700, 
+        width: '100%',
+        height: theme.size[14], 
+        paddingHorizontal: 16,
+        fontSize: theme.spacing.md,
+        color: theme.colors.white,
+        fontFamily: 'Roboto_400Regular',
+        marginBottom: 16,
+        borderRadius: 5,
+        borderWidth: focus ? 1 : 0,
+        borderColor: focus ? theme.colors.green_500 : 'transparent'
+      }}
+      placeholderTextColor={theme.colors.gray_300}
+      onFocus={() => setFocus(true)}
+      onBlur={() => setFocus(false)}
+      {...rest}
+    />
+  );
+}
