402e124dba894cee0dc6555ee0e95357718b1a86
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-09 21:53:55 -0300

    feat: finalizando tela de SingIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,15 +1,47 @@
-import { createBox, createText } from "@shopify/restyle";
+import { createBox, createText, useTheme } from "@shopify/restyle";
+import { Image } from "react-native";
+
 import { ThemeProps } from "src/theme";
+
+import LogoSvg from "@assets/logo.svg";
+import BackGroundImg from "@assets/background.png";
+import { Input } from "@components/Input";
 
 const Box = createBox<ThemeProps>();
 const Text = createText<ThemeProps>();
 
 export function SignIn(){
   return(
-    <Box flex={1} bg="gray_500" justifyContent="center" alignItems="center">
-      <Box flexDirection="row">
-        <Text variant="heading"> Hello World</Text>
-      </Box>  
+    <Box flex={1} bg="gray_700" paddingHorizontal="xs2">
+       <Image
+        source={BackGroundImg}
+        alt="Background img"
+        resizeMode="contain"
+        style={{position:"absolute"}}
+       />
+
+       <Box marginVertical="xxl" justifyContent="center" alignItems="center">
+        <LogoSvg />
+        <Text color="gray_100" variant="body">
+          Treine sua mente e seu corpo
+        </Text>
+       </Box>
+
+       <Box justifyContent="center" alignItems="center">
+        <Text variant="heading" marginBottom="xs">
+          Acesse sua conta
+        </Text>
+
+        <Input 
+         placeholder="E-mail"
+         keyboardType="email-address"
+         autoCapitalize="none"
+        />
+        <Input 
+         placeholder="Senha"
+         secureTextEntry
+        />
+       </Box>
     </Box>
   );
 }
