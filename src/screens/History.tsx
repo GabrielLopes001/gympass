543bfab31628b20782f6b0497241ef9f2797c602
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-15 21:09:27 -0300

    feat: finalizando tela de exercícios

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { createBox, createText } from "@shopify/restyle";
+import { useState } from "react";
+import { SectionList } from "react-native";
+import { ThemeProps } from "src/theme";
+
+const Box = createBox<ThemeProps>();
+const Text = createText<ThemeProps>();
 
 export function History(){
+  const [exercises, setExercises] = useState([
+    {
+      title: '15.01.24',
+      data: ['Puxada Lateral', 'Puxada unilateral'],
+    },
+    {
+      title: '14.01.24',
+      data: ['Puxada Lateral'],
+    }
+  ]);
+
   return(
-    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Text>
-        History
-      </Text>
-    </View>
+    <Box>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({section}) => (
+          <Text variant="body" mt="xs2" mb="md">
+            {section.title}
+          </Text>
+        )}
+        style={{paddingHorizontal: 32}}
+      />
+    </Box>
   )
 }
