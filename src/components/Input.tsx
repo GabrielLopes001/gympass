e4a6a35c1f6450632dd5167b3be61cec0d524675
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-22 22:43:02 -0300

    fix

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { TextInput, TextInputProps } from "react-native";
-import { useTheme } from "@shopify/restyle";
+import { createBox, createText, useTheme } from "@shopify/restyle";
 
 import { ThemeProps } from "src/theme";
 
@@ -9,31 +9,42 @@ type variantInputStyleProps ='PRIMARY' | 'SECONDARY';
 
 type Props = TextInputProps & {
   variant?: variantInputStyleProps;
+  errorMessage?: string | null;
 }
 
-export function Input({variant = 'PRIMARY', ...rest}: Props){
+const Text = createText<ThemeProps>();
+const Box = createBox<ThemeProps>();
+
+export function Input({variant = 'PRIMARY', errorMessage = null, ...rest}: Props){
   const { colors, spacing } = useTheme<ThemeProps>();
   const [focus, setFocus] = useState(false);
+  const invalid = !!errorMessage;
 
   return(
-    <TextInput
-      style={{
-        backgroundColor: variant === 'PRIMARY' ? colors.gray_700 : colors.gray_600, 
-        width: '100%',
-        height: spacing[14], 
-        paddingHorizontal: 16,
-        fontSize: spacing[4],
-        color: colors.white,
-        fontFamily: 'Roboto_400Regular',
-        marginBottom: 16,
-        borderRadius: 5,
-        borderWidth: focus ? 1 : 0,
-        borderColor: focus ? colors.green_500 : 'transparent'
-      }}
-      placeholderTextColor={colors.gray_300}
-      onFocus={() => setFocus(true)}
-      onBlur={() => setFocus(false)}
-      {...rest}
-    />
+    <Box  width="100%">
+      <TextInput
+        style={{
+          backgroundColor: variant === 'PRIMARY' ? colors.gray_700 : colors.gray_600, 
+          width: '100%',
+          height: spacing[14], 
+          paddingHorizontal: 16,
+          fontSize: spacing[4],
+          color: colors.white,
+          fontFamily: 'Roboto_400Regular',
+          marginBottom: 6,
+          borderRadius: 5,
+          borderWidth: focus ? 1 : invalid ? 1 : 0,
+          borderColor: focus ? colors.green_500 : invalid ? colors.red_500 : 'transparent'
+        }}
+        placeholderTextColor={colors.gray_300}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        {...rest}
+      />
+
+      <Text variant="body" color="red_500" mb="1">
+        {errorMessage}
+      </Text>
+    </Box>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createBox, useTheme } from "@shopify/restyle";
 import { ThemeProps } from "src/theme";
 
 import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
 const Box = createBox<ThemeProps>();
 
