48fb888de354809fed7051529e73b6099971bfd0
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-26 19:57:23 -0300

    feat: salvando e persistindo dados no dispositivo

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { ReactNode, createContext, useState } from 'react'
 
 import { api } from '@services/api';
 import { UserDTO } from '@dtos/UserDTO'
+import { storageUserSave } from '@storage/storageUser';
 
 export type AuthContextDataProps = {
    user: UserDTO;
@@ -24,6 +25,7 @@ export function AuthContextProvider({children}: AuthContextProviderProps){
 
          if(data.user){
             setUser(data.user)
+            storageUserSave(data.user)
          }
       } catch (error) {
          throw error
