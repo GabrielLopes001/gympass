47053cc7811eecd2a7abc4320e5bb5ac175ab82e
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-26 19:58:15 -0300

    feat: redirecionando usuario para aplicação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,16 +11,15 @@ const Box = createBox<ThemeProps>();
 export function Routes(){
   const { colors } = useTheme<ThemeProps>();
   const { user } = useAuth();
-  console.log('LOGADO => ', user)
+  console.log('USUARIO LOGADO =>', user)
   
   const theme = DefaultTheme
-  
   theme.colors.background = colors.gray_700
 
   return(
     <Box flex={1} bg="gray_700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
       
