45f9ea3bfa0ab24dc1bb5acacf64e59598404661
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-26 21:48:19 -0300

    feat: adicionando função de busca e remoção do dispositivo

--- src/storage/storageUser.ts
+++ src/storage/storageUser.ts
@@ -1,7 +1,20 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { UserDTO } from "@dtos/UserDTO";
 import { USER_STORAGE } from "./storageConfig";
 
 export async function storageUserSave(user: UserDTO) {
    AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user))
+}
+
+export async function storageUserGet() {
+   const storageUser = await AsyncStorage.getItem(USER_STORAGE) 
+   
+   const user: UserDTO = storageUser ? JSON.parse(storageUser) : {}
+
+   return user
+}
+
+export async function storageUserRemove() {
+   AsyncStorage.removeItem(USER_STORAGE)
 }
