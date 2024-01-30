f9c630554b8a64fb55b45c2ac9e1551b3a6b694f
GabrielLopes001 <gabrieljob1@outlook.com>
2024-01-29 23:23:28 -0300

    feat: Buscando Histórico na API e Listando

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,3 +1,4 @@
+import { HistoryDTO } from "@dtos/HistoryDTO";
 import { createBox, createText } from "@shopify/restyle"
 import { ThemeProps } from "src/theme"
 
@@ -5,20 +6,24 @@ import { ThemeProps } from "src/theme"
 const Box = createBox<ThemeProps>();
 const Text = createText<ThemeProps>();
 
-export function HistoryCard(){
+type HistoryExerciseProps = {
+   data: HistoryDTO;
+}
+
+export function HistoryCard({data}: HistoryExerciseProps){
    return(
       <Box width="100%" flexDirection="row" px="4" py="4" mb="3" borderRadius={5} bg="gray_500" alignItems="center" justifyContent="space-between">
          <Box mr="3" flex={1}>
             <Text variant="heading" fontSize={16} textTransform="capitalize">
-               Costas
+               {data.group}
             </Text>
             <Text variant="body" numberOfLines={1} fontSize={18}>
-               Puxa Frontal
+               {data.name}
             </Text>
          </Box>
 
          <Text variant="body" color="gray_300">
-            08:20
+            {data.hour}
          </Text>
 
       </Box>
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
-import { SectionList } from "react-native";
+import { useCallback, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import { Alert, SectionList } from "react-native";
 import { createBox, createText } from "@shopify/restyle";
 
 import { ThemeProps } from "src/theme";
+
+import { api } from "@services/api";
+import { AppError } from "@utils/appError";
+import { HistoryByDayDTO } from "@dtos/HistoryByDayDTO";
 
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
@@ -11,16 +16,26 @@ const Box = createBox<ThemeProps>();
 const Text = createText<ThemeProps>();
 
 export function History(){
-  const [exercises, setExercises] = useState([
-    {
-      title: '15.01.24',
-      data: ['Puxada Lateral', 'Puxada unilateral'],
-    },
-    {
-      title: '14.01.24',
-      data: ['Puxada Lateral'],
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([]);
+
+  async function fetchExerciseHistory(){
+    try {
+      setIsLoading(true)
+      const response = await api.get('history')
+      setExercises(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível carregar o histórico'
+      Alert.alert('Erro',title)
+    } finally {
+      setIsLoading(false)
     }
-  ]);
+  }
+  
+  useFocusEffect(useCallback(() => {
+    fetchExerciseHistory()
+  },[]))
 
   return(
     <Box>
@@ -28,9 +43,11 @@ export function History(){
 
       <SectionList
         sections={exercises}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({item}) => (
-          <HistoryCard />
+          <HistoryCard
+            data={item}
+          />
         )}
         renderSectionHeader={({section}) => (
           <Text variant="body" mt="10" mb="4">
